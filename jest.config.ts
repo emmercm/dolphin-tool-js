57a85dc4ba5f03aaa7f45f45541899122d633dd2
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2025-02-16 18:18:21 -0800

    Revert "testTimeout"
    This reverts commit b089f32c71ae184b8e1b6046e8de760e375619ca.

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,6 @@ import { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   testEnvironment: 'node',
 
-  // Windows loves to take a long time?
-  testTimeout: 10_000,
-
   // BEGIN https://kulshekhar.github.io/ts-jest/docs/guides/esm-support
   extensionsToTreatAsEsm: ['.ts'],
   transform: {
