ae2f7e5f2b4aa34f5669f59404148a57275a60eb
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2025-02-25 22:49:44 -0500

    Fix: use temporary user folders on Windows (#2)

--- packages/dolphin-tool/src/dolphin-tool/dolphinToolConvert.ts
+++ packages/dolphin-tool/src/dolphin-tool/dolphinToolConvert.ts
@@ -1,5 +1,6 @@
 import DolphinToolBin, { DolphinToolRunOptions } from './dolphinToolBin.js';
 import { CompressionMethodWiaRvz, ContainerFormat } from './common.js';
+import utils from './utils.js';
 
 export interface CreateOptions extends DolphinToolRunOptions {
   inputFilename: string,
@@ -14,9 +15,8 @@ export interface CreateOptions extends DolphinToolRunOptions {
 
 export default {
   async convert(options: CreateOptions): Promise<void> {
-    await DolphinToolBin.run([
+    const runOptions: string[] = [
       'convert',
-      ...(options.userFolderPath ? ['-u', options.userFolderPath] : []),
       '-i', options.inputFilename,
       '-o', options.outputFilename,
       '-f', options.containerFormat,
@@ -24,6 +24,28 @@ export default {
       ...(options.blockSize === undefined ? [] : ['-b', String(options.blockSize)]),
       ...(options.compressionMethod === undefined ? [] : ['-c', options.compressionMethod]),
       ...(options.compressionLevel === undefined ? [] : ['-l', String(options.compressionLevel)]),
+    ];
+
+    if (process.platform === 'win32' && options.userFolderPath === undefined) {
+      /**
+       * Windows (and seemingly no other OS) has issues with concurrent or rapid execution of
+       * `DolphinTool.exe`, resulting in popups with messages such as:
+       *    "IOS_FS: Failed to write new FST"
+       *    "IOS_FS: Failed to rename temporary FST file"
+       *    "File <userDir>/Wii/shared2/sys/SYSCONF could not be opened! This may happen with
+       *      improper permissions or use by another process."
+       * To combat this, we have to use separate user directories per process.
+       */
+      await utils.wrapTempDir(async (temporaryDirectory) => DolphinToolBin.run([
+        ...runOptions,
+        '-u', temporaryDirectory,
+      ], options));
+      return;
+    }
+
+    await DolphinToolBin.run([
+      ...runOptions,
+      ...(options.userFolderPath ? ['-u', options.userFolderPath] : []),
     ], options);
   },
 };
