1c2c04614087fc388069379f8d7177bcbcb17d11
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-03-07 18:31:57 -0800

    Feature: use Bun-bundled binaries (#31)

--- packages/dolphin-tool/src/dolphin-tool/dolphinToolBin.ts
+++ packages/dolphin-tool/src/dolphin-tool/dolphinToolBin.ts
@@ -2,6 +2,11 @@ import which from 'which';
 import util from 'node:util';
 import fs from 'node:fs';
 import * as child_process from 'node:child_process';
+import { Mutex } from 'async-mutex';
+import crypto from 'node:crypto';
+import path from 'node:path';
+import stream from 'node:stream';
+import os from 'node:os';
 
 export interface DolphinToolRunOptions {
   binaryPreference?: DolphinToolBinaryPreference
@@ -19,6 +24,8 @@ export enum DolphinToolBinaryPreference {
 export default class DolphinToolBin {
   private static DOLPHIN_TOOL_BIN: string | undefined;
 
+  private static readonly DOLPHIN_TOOL_BIN_MUTEX = new Mutex();
+
   private static async getBinPath(
     binaryPreference?: DolphinToolBinaryPreference,
   ): Promise<string | undefined> {
@@ -26,29 +33,125 @@ export default class DolphinToolBin {
       return this.DOLPHIN_TOOL_BIN;
     }
 
-    if ((binaryPreference ?? DolphinToolBinaryPreference.PREFER_BUNDLED_BINARY)
-      === DolphinToolBinaryPreference.PREFER_BUNDLED_BINARY
-    ) {
-      const pathBundled = await this.getBinPathBundled();
-      this.DOLPHIN_TOOL_BIN = pathBundled ?? (await this.getBinPathExisting());
-    } else {
-      const pathExisting = await this.getBinPathExisting();
-      this.DOLPHIN_TOOL_BIN = pathExisting ?? (await this.getBinPathBundled());
-    }
+    return this.DOLPHIN_TOOL_BIN_MUTEX.runExclusive(async () => {
+      if (this.DOLPHIN_TOOL_BIN) {
+        return this.DOLPHIN_TOOL_BIN;
+      }
 
-    return this.DOLPHIN_TOOL_BIN;
+      if ((binaryPreference ?? DolphinToolBinaryPreference.PREFER_BUNDLED_BINARY)
+      === DolphinToolBinaryPreference.PREFER_BUNDLED_BINARY
+      ) {
+        const pathBundled = await this.getBinPathBundled();
+        this.DOLPHIN_TOOL_BIN = pathBundled ?? (await this.getBinPathExisting());
+      } else {
+        const pathExisting = await this.getBinPathExisting();
+        this.DOLPHIN_TOOL_BIN = pathExisting ?? (await this.getBinPathBundled());
+      }
+
+      return this.DOLPHIN_TOOL_BIN;
+    });
   }
 
   private static async getBinPathBundled(): Promise<string | undefined> {
+    const bunPath = await this.getBinPathBundledBun();
+    if (bunPath !== undefined) {
+      return bunPath;
+    }
+
     try {
       const dolphinTool = await import(`@emmercm/dolphin-tool-${process.platform}-${process.arch}`);
       const prebuilt = dolphinTool.default;
-      if (await util.promisify(fs.exists)(prebuilt)) {
+      try {
+        await util.promisify(fs.stat)(prebuilt);
         return prebuilt;
+      } catch {
+        /* ignored */
       }
     } catch { /* ignored */ }
 
     return undefined;
+  }
+
+  /**
+   * Look for dolphin-tool binaries bundled with:
+   * `bun build --compile --asset-naming="[name].[ext]" dolphin-tool *.dylib`
+   */
+  private static async getBinPathBundledBun(): Promise<string | undefined> {
+    try {
+      const { embeddedFiles } = await import('bun');
+
+      // Find all files that might be dolphin-tool-related
+      const dolphinToolBlob = embeddedFiles.find((blob) => {
+        // @ts-expect-error https://github.com/oven-sh/bun/issues/20700
+        const blobName: string = blob.name;
+        return blobName.toLowerCase().startsWith('dolphin');
+      });
+
+      if (dolphinToolBlob !== undefined) {
+        // Create the temporary directory
+        const hash = crypto.createHash('md5');
+        await dolphinToolBlob.stream().pipeTo(new WritableStream({
+          write(chunk): void {
+            hash.update(chunk);
+          },
+        }));
+        const temporaryDirectory = await this.getTemporaryDirectory(`dolphin-tool-${hash.digest('hex').slice(0, 7)}`);
+
+        // Find additional files that might be necessary
+        const dylibBlobs = embeddedFiles.filter((blob) => {
+          // @ts-expect-error https://github.com/oven-sh/bun/issues/20700
+          const blobName: string = blob.name;
+          return blobName.toLowerCase().endsWith('.dylib');
+        });
+
+        // Extract all files if necessary
+        const temporaryBlobs = await Promise.all(
+          [dolphinToolBlob, ...dylibBlobs].map(async (blob) => {
+          // @ts-expect-error https://github.com/oven-sh/bun/issues/20700
+            const blobName: string = blob.name.replace(/-[\da-z]{8}\./, '.').replace(/\.+$/, '');
+            const temporaryBlob = path.join(temporaryDirectory, blobName);
+            try {
+              await util.promisify(fs.stat)(temporaryBlob);
+              return temporaryBlob;
+            } catch {
+            /* ignored */
+            }
+            const writableStream = stream.Writable.toWeb(fs.createWriteStream(temporaryBlob));
+            await blob.stream().pipeTo(writableStream);
+            await util.promisify(fs.chmod)(temporaryBlob, 0o755); // chmod +x
+            return temporaryBlob;
+          }),
+        );
+        return temporaryBlobs.find((temporaryBlob) => path.basename(temporaryBlob).startsWith('dolphin'));
+      }
+    } catch { /* ignored */ }
+
+    return undefined;
+  }
+
+  private static async getTemporaryDirectory(temporaryDirectoryBasename: string): Promise<string> {
+    const candidateDirectories = [
+      path.join(os.tmpdir(), temporaryDirectoryBasename),
+      path.join(process.cwd(), '.dolphin-tool', temporaryDirectoryBasename),
+      path.join(os.homedir(), '.dolphin-tool', temporaryDirectoryBasename),
+    ];
+    /* eslint-disable no-await-in-loop */
+    for (const candidateDirectory of candidateDirectories) {
+      try {
+        try {
+          await util.promisify(fs.stat)(candidateDirectory);
+        } catch {
+          await util.promisify(fs.mkdir)(candidateDirectory, { recursive: true });
+        }
+        const temporaryFile = path.join(candidateDirectory, temporaryDirectoryBasename);
+        await util.promisify(fs.writeFile)(temporaryFile, temporaryFile);
+        await util.promisify(fs.unlink)(temporaryFile);
+        return candidateDirectory;
+      } catch {
+        /* ignored */
+      }
+    }
+    throw new Error("couldn't find a suitable temporary directory");
   }
 
   private static async getBinPathExisting(): Promise<string | undefined> {
