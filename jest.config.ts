f4a0d1f4f1f0cd7745ee421186d7131e065ccb40
Christian Emmer <10749361+emmercm@users.noreply.github.com>
2026-03-26 20:36:12 -0700

    testTimeout: 10_000

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import { JestConfigWithTsJest } from 'ts-jest';
 const jestConfig: JestConfigWithTsJest = {
   testEnvironment: 'node',
 
+  testTimeout: 10_000,
+
   // BEGIN https://kulshekhar.github.io/ts-jest/docs/guides/esm-support
   extensionsToTreatAsEsm: ['.ts'],
   transform: {
